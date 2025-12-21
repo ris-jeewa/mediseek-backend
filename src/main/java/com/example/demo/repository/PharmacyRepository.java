4c693cc2c0338cd3213875e904607faded8a12e2
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2025-12-21 09:54:33 +0530

    Add existsByRegistrationNumber method to PharmacyRepository for registration number validation

--- src/main/java/com/example/demo/repository/PharmacyRepository.java
+++ src/main/java/com/example/demo/repository/PharmacyRepository.java
@@ -4,4 +4,5 @@ import com.example.demo.entity.Pharmacy;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PharmacyRepository extends JpaRepository<Pharmacy, Long> {
+     Boolean existsByRegistrationNumber(String registrationNumber); 
 }
