93766b60903be17be44e2ad18ded5ae655c2df47
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2026-01-15 16:15:06 +0530

    fix: remove delted dto import

--- src/main/java/com/example/demo/controller/DoctorController.java
+++ src/main/java/com/example/demo/controller/DoctorController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.dto.DoctorHospitalsResponseDTO;
 import com.example.demo.entity.Hospital;
 import com.example.demo.service.DoctorHospitalService;
 
