dfd517aa9b3dbdee5043e54f600805ac32a04fb7
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2026-01-16 18:33:08 +0530

    feat: rather than API request call data include DB data DTO changed

--- src/main/java/com/example/demo/dto/PaginatedHospitalResponse.java
+++ src/main/java/com/example/demo/dto/PaginatedHospitalResponse.java
@@ -2,13 +2,15 @@ package com.example.demo.dto;
 
 import java.util.List;
 
+import com.example.demo.entity.Hospital;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class PaginatedHospitalResponse {
-    private List<HospitalDTO> content;
+    private List<Hospital> content;
     private int page;
     private int size;
     private long totalElements;
